5edda7684617137de1c31a991dff3ca4e8e129da
fengpengfei111 <1479623700@qq.com>
2022-04-14 14:13:12 +0800

    更新

---
+++ userservice02/src/test/java/com/example/userservice02/Userservice02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userservice02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Userservice02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
